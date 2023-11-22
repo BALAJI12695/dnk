874201b2e1839f3a77bf006997d49254027c4202
BALAJI12695 <142472306+BALAJI12695@users.noreply.github.com>
2023-11-22 20:42:20 +0530

    ham nav toggle

--- src/Components/Ham.jsx
+++ src/Components/Ham.jsx
@@ -9,11 +9,23 @@ export const Ham = () => {
         <nav class="navbar navbar-expand-lg bg">
         <div class="container-fluid">
         <Link to='/'><div class="navbar-brand" href="#"></div></Link>
+        <div class=" d-flex ">
+        <div className='nav-cart'>
+        <ul class="navbar-nav flex-row " >
+        <li class="nav-item active " ><Link class="nav-link" to='/cart'> <div>$00.00</div></Link></li>
+        <li class="nav-item active "><Link class="nav-link" to='/wishlist' ><Icon.SuitHeartFill  size="20" /></Link></li>
+        <li class="nav-item active "><Link class="nav-link" to='/cart' ><Icon.BagFill  size="20" /></Link></li>
+        <li class="nav-item active "><div className="nav-cart-count">0</div></li> 
+        </ul>
+        </div>
+        <div>
         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
         <span class="navbar-toggler-icon"></span>
         </button>
+        </div>
+        </div>
         <div class="collapse navbar-collapse" id="navbarText">
-        <ul class="navbar-nav me-auto mb-4 mb-lg-0">
+        <ul class="navbar-nav me-auto mb-4 mb-lg-0  ">
         <li class="nav-item active"><Link class="nav-link" to='/'>EVERYTHING</Link></li>
         <li class="nav-item active"><Link class="nav-link" to='/women'>WOMEN</Link></li>
         <li class="nav-item active"><Link class="nav-link" to='/men'>MEN</Link></li>  
@@ -25,13 +37,14 @@ export const Ham = () => {
         <li class="nav-item active"><Link class="nav-link" to='/about' >ABOUT</Link></li>
         <li class="nav-item active"><Link class="nav-link" to='/contact' ><p>CONTACT US</p></Link></li>
         <li class="nav-item active" ><Link class="nav-link" to='/cart'> <div>$00.00</div> </Link> </li>
-        <li class="nav-item active"><Link class="nav-link" to='/wishlist' ><Icon.SuitHeartFill className="align-top" size="20" /></Link></li>
-        <li class="nav-item active"><Link class="nav-link" to='/cart' ><Icon.BagFill className="align-top" size="20" /></Link></li>
+        <li class="nav-item active"><Link class="nav-link" to='/wishlist' ><Icon.SuitHeartFill  size="20" /></Link></li>
+        <li class="nav-item active"><Link class="nav-link" to='/cart' ><Icon.BagFill  size="20" /></Link></li>
         <li class="nav-item active"><div className="nav-cart-count">0</div></li>
-        <li class="nav-item active"><Link class="nav-link" to='/login' ><Icon.PersonFill className="align-top" size="24" /></Link></li> 
+        <li class="nav-item active"><Link class="nav-link" to='/login' ><Icon.PersonFill  size="25" /></Link></li> 
         </span>
       </ul>
       </div>
+      
     </div>
 </nav>
 </div>
